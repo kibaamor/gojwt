b5102fa0aae4f6014b7cac6b7dc07d3e6a3562a6
Kiba Amor <5093911+kibaamor@users.noreply.github.com>
2023-01-12 17:40:33 +0800

    move pkcs7 function into utils

--- utils.go
+++ utils.go
@@ -1,10 +1,39 @@
 package gojwt
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"os"
 )
+
+var errPKCS7Padding = errors.New("gojwt/pkcs7: invalid padding")
+
+func PKCS7Padding(data []byte, blockSize int) []byte {
+	padding := blockSize - len(data)%blockSize
+	return append(data, bytes.Repeat([]byte{byte(padding)}, padding)...)
+}
+
+func PKCS7Unpadding(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, errPKCS7Padding
+	}
+
+	padding := int(data[len(data)-1])
+	if len(data) < padding {
+		return nil, errPKCS7Padding
+	}
+
+	// make sure the values of all padding bytes are equal to 'padding'
+	for i := len(data) - padding + 1; i < len(data); i++ {
+		if data[i-1] != data[i] {
+			return nil, errPKCS7Padding
+		}
+	}
+
+	return data[:len(data)-padding], nil
+}
 
 func RawURLEncode(src []byte) []byte {
 	if len(src) == 0 {
--- utils_test.go
+++ utils_test.go
@@ -8,6 +8,102 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPKCS7Padding(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   []byte
+		padding int
+		want    []byte
+	}{
+		{
+			name:    "0 bytes",
+			input:   []byte{},
+			padding: 4,
+			want:    []byte{4, 4, 4, 4},
+		},
+		{
+			name:    "1 bytes",
+			input:   []byte{8},
+			padding: 4,
+			want:    []byte{8, 3, 3, 3},
+		},
+		{
+			name:    "2 bytes",
+			input:   []byte{8, 8},
+			padding: 4,
+			want:    []byte{8, 8, 2, 2},
+		},
+		{
+			name:    "3 bytes",
+			input:   []byte{8, 8, 8},
+			padding: 4,
+			want:    []byte{8, 8, 8, 1},
+		},
+		{
+			name:    "4 bytes",
+			input:   []byte{8, 8, 8, 8},
+			padding: 4,
+			want:    []byte{8, 8, 8, 8, 4, 4, 4, 4},
+		},
+		{
+			name:    "5 bytes",
+			input:   []byte{8, 8, 8, 8, 8},
+			padding: 4,
+			want:    []byte{8, 8, 8, 8, 8, 3, 3, 3},
+		},
+	}
+
+	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := PKCS7Padding(tt.input, tt.padding)
+			assert.Equal(t, tt.want, got)
+
+			output, err := PKCS7Unpadding(got)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.input, output)
+		})
+	}
+}
+
+func TestPKCS7Unpadding(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []byte
+	}{
+		{
+			name:  "nil input",
+			input: nil,
+		},
+		{
+			name:  "zero length",
+			input: []byte{},
+		},
+		{
+			name:  "invalid padding length",
+			input: []byte{2},
+		},
+		{
+			name:  "invalid padding values",
+			input: []byte{1, 2},
+		},
+		{
+			name:  "unequal padding values",
+			input: []byte{8, 1, 2},
+		},
+	}
+
+	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := PKCS7Unpadding(tt.input)
+			if !assert.NotNil(t, err) {
+				assert.Equal(t, errPKCS7Padding, err)
+			}
+		})
+	}
+}
+
 func TestRandBytes(t *testing.T) {
 	tests := []struct {
 		name  string
